cf8085d7c3ad5d200a73b896fc1f62612c5a26e6
devanych <mail@devanych.ru>
2020-09-29 19:12:57 +0300

    Extend types for InvalidMiddlewareResolverHandler

--- src/Exception/InvalidMiddlewareResolverHandlerException.php
+++ src/Exception/InvalidMiddlewareResolverHandlerException.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace HttpSoft\Runner\Exception;
 
 use InvalidArgumentException;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 use function gettype;
 use function get_class;
 use function is_object;
+use function is_scalar;
 use function sprintf;
 
 class InvalidMiddlewareResolverHandlerException extends InvalidArgumentException
@@ -20,8 +23,11 @@ class InvalidMiddlewareResolverHandlerException extends InvalidArgumentException
     public static function create($handler): self
     {
         return new self(sprintf(
-            '`%s` is not a valid handler.',
-            (is_object($handler) ? get_class($handler) : gettype($handler))
+            'Handler "%s" must be an instance or name of a class that implements `%s` or `%s`'
+            . ' interface as well as a PHP callable or array of such arguments.',
+            self::convertToString($handler),
+            MiddlewareInterface::class,
+            RequestHandlerInterface::class
         ));
     }
 
@@ -32,8 +38,32 @@ class InvalidMiddlewareResolverHandlerException extends InvalidArgumentException
     public static function forCallableMissingResponse($response): self
     {
         return new self(sprintf(
-            'Callable middleware must returned an instance of `Psr\Http\Message\ResponseInterface`; received `%s`.',
-            (is_object($response) ? get_class($response) : gettype($response))
+            'Callable handler must returned an instance of `Psr\Http\Message\ResponseInterface`; received "%s".',
+            self::convertToString($response)
         ));
+    }
+
+    /**
+     * @param string $handler
+     * @return self
+     */
+    public static function forStringNotConvertedToInstance(string $handler): self
+    {
+        return new self(sprintf(
+            'String handler "%s" must be a name of a class or an identifier of'
+            . ' a container definition that implements `%s` or `%s` interface.',
+            $handler,
+            MiddlewareInterface::class,
+            RequestHandlerInterface::class
+        ));
+    }
+
+    /**
+     * @param mixed $data
+     * @return string
+     */
+    private static function convertToString($data): string
+    {
+        return is_scalar($data) ? (string) $data : (is_object($data) ? get_class($data) : gettype($data));
     }
 }
